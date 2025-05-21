c83a9c39254ff76e2330ea4f436e178e44bdb8c5
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-05-21 14:49:05 +0300

    Add production CORS origin for deployment on Vercel

--- backend/blogify_backend/blogify_backend/settings.py
+++ backend/blogify_backend/blogify_backend/settings.py
@@ -71,7 +71,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://blogify-alpha-six.vercel.app'
 ]
 
 CORS_ALLOW_METHODS = [
